b7fe78754920a4ac2852d65c553dc2cf12e3d3c4
gabogg21 <gabrielgiron@ucb.edu.bo>
2025-03-13 12:32:29 -0400

    examen

---
+++ primerParcial231/tarea3/basicScript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Bienvenido al script del examen"
+echo "Porfavor ingrese su nombre:"
+read nombre
+echo "Hola, $nombre"
+echo "El contenido de la carpeta tarea3 es:"
+tree
+echo "Ha finalizado el script"
